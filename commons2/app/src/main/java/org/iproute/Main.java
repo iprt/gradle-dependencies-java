3105d5fc9a3a24ec1b3553affd6f18b19ebe49ac
zhuzhenjie <mimotronik@gmail.com>
2023-09-08 14:49:08 +0800

    ft: api

---
+++ commons2/app/src/main/java/org/iproute/Main.java
@@ -0,0 +1,17 @@
+package org.iproute;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Main
+ *
+ * @author zhuzhenjie
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Class<?> clazz = StringUtils.class;
+
+        System.out.println(clazz);
+    }
+}
---
+++ commons2/utils/src/main/java/org/iproute/utils/HelloWorld.java
@@ -0,0 +1,16 @@
+package org.iproute.utils;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * HelloWorld
+ *
+ * @author zhuzhenjie
+ */
+public class HelloWorld {
+
+    public static boolean isBlank(CharSequence cs) {
+        return StringUtils.isBlank(cs);
+    }
+
+}
