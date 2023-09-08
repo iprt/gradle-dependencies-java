2536a10e20c5593b4f946d3da409ae1fa91f78b3
zhuzhenjie <mimotronik@gmail.com>
2023-09-08 14:52:28 +0800

    fx

--- commons2/app/src/main/java/org/iproute/Main.java
+++ commons2/app/src/main/java/org/iproute/Main.java
@@ -2,6 +2,9 @@ package org.iproute;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * Main
  *
@@ -13,5 +16,14 @@ public class Main {
         Class<?> clazz = StringUtils.class;
 
         System.out.println(clazz);
+
+
+        Properties p = new Properties();
+        try {
+            p.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("config.properties"));
+            System.out.println(p.getProperty("name"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
